5d507ec606d8d1e7e23d0f07556753ae45ce3db2
ScaredCube <74418739+ScaredCube@users.noreply.github.com>
2024-06-09 11:52:23 +0800

    python marginal notes

--- .github/scripts/docs_translate2tw.py
+++ .github/scripts/docs_translate2tw.py
@@ -7,28 +7,27 @@ import json
 converter = opencc.OpenCC('s2twp.json')
 
 # Get the GitHub token and repository information from environment variables
-# Get the GitHub token and repository information from environment variables
 token = os.getenv('GITHUB_TOKEN')
 repo_name = os.getenv('GITHUB_REPOSITORY')
 
-# Read event data to get the PR number
+# Get the PR number
 with open(os.getenv('GITHUB_EVENT_PATH')) as f:
     event_data = json.load(f)
 pr_number = event_data['number']
 
-# Get the list of files changed in the pull request
+# Get the list of files changed
 headers = {'Authorization': f'token {token}'}
 url = f'https://api.github.com/repos/ScaredCube/travelling-test/pulls/{pr_number}/files'
 response = requests.get(url, headers=headers)
 
-# Check for errors in the API response
+# Catching errors
 if response.status_code != 200:
     print(f"Failed to fetch PR files: {response.status_code} {response.text} \nUrl: {url}")
     exit(1)
 
 files = response.json()
 
-# Filter docs, announcements, and blog files and translate their content
+# Translate docs
 translations = []
 directories_to_check = ['docs/', 'announcements/', 'blog/']
 for file in files:
